b88096504751668f2f191f0faff672982b359383
syuan0212@gmail.com <hsuan777>
2020-04-15 22:55:57 +0800

    修改__JS__依序列出結果

--- js/all.js
+++ js/all.js
@@ -1,28 +1,60 @@
 
 
-joyjoy();
+// console.log(x + ' x ' + i + ' = ' + x * i);
+// 可以一次印出結果，但是無法依序列出結果
+// function joy(num) {
+//   for (let y = 1; y < 10; y++) {
+//     console.log(`${num} x ${y} = ${num * y}`);
+//   }
+// };
 
-function joyjoy(){
-  for(let i = 1; i < 10; i++){
-   let content = document.querySelector('.content');
-   let contentItem = document.createElement('div')
-   contentItem.classList.add('content__item');
-   let text = document.createElement('p');
-   contentItem.textContent = `<p>${joy(1)}</p><br>`;
-   content.appendChild(contentItem);
-   console.log(joy(1)); 
-//    console.log(joy(i)); 
-  }
+// 會依序列出結果
+for (var i = 1; i < 5; i++) {
+  (function (x) {
+    console.log(`${x}`);
+  })(i);
+}
+
+let content = document.querySelector('.content');
+for (var i = 1; i < 10; i++) {
+  let contentItem = document.createElement('div');
+  let text = document.createElement('p');
+  contentItem.classList.add('content__item');
+  (function (num) {
+    for (let y = 1; y < 10; y++) {
+      // console.log(`${num} x ${y} = ${num * y}`);
+      (function (y){
+        text.textContent = `${num} x ${y} = ${num * y}`;
+
+      })(y);
+    }
+  })(i);
+  content.appendChild(contentItem).appendChild(text);
 }
 
 
-// 1~9
-function joy(num) {
+
+
+
+function joyjoy() {
+  let content = document.querySelector('.content');
   for (let i = 1; i < 10; i++) {
-    let x = num;
-    console.log(x + ' x ' + i + ' = ' + x * i);
+    let contentItem = document.createElement('div');
+    contentItem.classList.add('content__item');
+    let text = document.createElement('p');
+    (function (num) {
+      for (let y = 1; y < 10; y++) {
+        text.textContent = `${num} x ${y} = ${num * y}`;
+      }
+    })(i);
+    content.appendChild(contentItem).appendChild(text);
+
   }
 }
+
+
+
+
 
 
 
