34b5d4d660c06cca25f09e60157302599b0d0f17
Vic <Vic@gmail.com>
2020-04-16 15:31:53 +0800

    修改__content__item內容與樣式

--- js/all.js
+++ js/all.js
@@ -8,52 +8,56 @@
 //   }
 // };
 
-// 會依序列出結果
-for (var i = 1; i < 5; i++) {
-  (function (x) {
-    console.log(`${x}`);
-  })(i);
-}
+// 可依序列出結果，IIFE 立刻執行
+// for (var i = 1; i < 10; i++) {
+//   (function (x) {
+//     console.log(`${x}`);
+//   })(i);
+// }
 
+
+// IIFE 立刻執行，可加上 setTimeout
+// setTimeout(function(){},1000)
+// 跑 2~9 ，共 8 個 item ， 1 個 item 內有 9 個 <p>
 let content = document.querySelector('.content');
-for (var i = 1; i < 10; i++) {
+for (var i = 2; i < 10; i++) {
   let contentItem = document.createElement('div');
-  let text = document.createElement('p');
-  contentItem.classList.add('content__item');
+  let contentItem__title = document.createElement('h2');
+  contentItem__title.classList.add('content__item__title');
+  contentItem__title.textContent = i;
+  contentItem.appendChild(contentItem__title);
+  contentItem.classList.add('content__item','d-flex');
   (function (num) {
     for (let y = 1; y < 10; y++) {
-      // console.log(`${num} x ${y} = ${num * y}`);
-      (function (y){
-        text.textContent = `${num} x ${y} = ${num * y}`;
-
+      (function (num__sec){
+        let text = document.createElement('p');
+        text.classList.add('content__item__text');
+        text.textContent = `${num} x ${num__sec} = ${num * num__sec}`;
+        contentItem.appendChild(text);
       })(y);
     }
   })(i);
-  content.appendChild(contentItem).appendChild(text);
+  content.appendChild(contentItem);
 }
 
+// 一次印出結果
+// joy_pri();
+// function joy_pri() {
+//   let content = document.querySelector('.content');
+//   for (let i = 2; i < 10; i++) {
+//     let contentItem = document.createElement('div');
+//     contentItem.classList.add('content__item');
+//     for (let y = 1; y < 10; y++) {
+//       let text = document.createElement('p');
+//       contentItem.appendChild(text);
+//       text.textContent = `${i} x ${y} = ${i * y}`;
+//     }
+//     content.appendChild(contentItem);
 
+//   }
+// }
 
-
-
-function joyjoy() {
-  let content = document.querySelector('.content');
-  for (let i = 1; i < 10; i++) {
-    let contentItem = document.createElement('div');
-    contentItem.classList.add('content__item');
-    let text = document.createElement('p');
-    (function (num) {
-      for (let y = 1; y < 10; y++) {
-        text.textContent = `${num} x ${y} = ${num * y}`;
-      }
-    })(i);
-    content.appendChild(contentItem).appendChild(text);
-
-  }
-}
-
-
-
+// window.setTimeout(joy_pri(),5000);
 
 
 
