3a0e20b295f9933c548ee7e4af8e95cb4827fd68
syuan0212@gmail.com <hsuan777>
2020-04-16 22:08:15 +0800

    修改註解

--- js/all.js
+++ js/all.js
@@ -1,22 +1,5 @@
 
 
-// console.log(x + ' x ' + i + ' = ' + x * i);
-// 可以一次印出結果，但是無法依序列出結果
-// function joy(num) {
-//   for (let y = 1; y < 10; y++) {
-//     console.log(`${num} x ${y} = ${num * y}`);
-//   }
-// };
-
-// 可依序列出結果，IIFE 立刻執行
-// for (var i = 1; i < 10; i++) {
-//   (function (x) {
-//     console.log(`${x}`);
-//   })(i);
-// }
-
-
-// IIFE 立刻執行，可加上 setTimeout
 // setTimeout(function(){},1000)
 // 跑 2~9 ，共 8 個 item ， 1 個 item 內有 9 個 <p>
 let content = document.querySelector('.content');
@@ -26,7 +9,8 @@ for (var i = 2; i < 10; i++) {
   contentItem__title.classList.add('content__item__title');
   contentItem__title.textContent = i;
   contentItem.appendChild(contentItem__title);
-  contentItem.classList.add('content__item','d-flex');
+  contentItem.classList.add('content__item');
+  
   (function (num) {
     for (let y = 1; y < 10; y++) {
       (function (num__sec){
@@ -37,7 +21,9 @@ for (var i = 2; i < 10; i++) {
       })(y);
     }
   })(i);
-  content.appendChild(contentItem);
+  setTimeout(function(){
+    content.appendChild(contentItem);
+  },1000*i)
 }
 
 // 一次印出結果
@@ -49,16 +35,26 @@ for (var i = 2; i < 10; i++) {
 //     contentItem.classList.add('content__item');
 //     for (let y = 1; y < 10; y++) {
 //       let text = document.createElement('p');
-//       contentItem.appendChild(text);
 //       text.textContent = `${i} x ${y} = ${i * y}`;
+//       contentItem.appendChild(text);
 //     }
+    
 //     content.appendChild(contentItem);
 
 //   }
 // }
 
-// window.setTimeout(joy_pri(),5000);
+// 測試區
+// 可以一次印出結果，但是無法依序列出結果
+// function joy(num) {
+//   for (let y = 1; y < 10; y++) {
+//     console.log(`${num} x ${y} = ${num * y}`);
+//   }
+// };
 
-
-
-
+// 若加上 setTimeout可依序列出結果，IIFE 立刻執行
+// for (var i = 1; i < 10; i++) {
+//   (function (x) {
+//     console.log(`${x}`);
+//   })(i);
+// }
